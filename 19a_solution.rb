8e7f9daf834a412e14fb2320ead8a3e9bcca42e0
Pierpaolo Frasa <pfrasa@gmail.com>
2021-12-20 02:50:29 +0100

    day 19, part 1, speedup but still wrong

--- 19a_solution.rb
+++ 19a_solution.rb
@@ -1,67 +1,47 @@
 require 'set'
 
-scanners = [
-  [[0,2],[4,1],[3,3]],
-  [[-1,-1],[-5,0],[-2,1]]
-]
-
-scanner_data = Array.new(scanners.length) { [] }
-scanner_data[0] << { from: 0, pos: [0,0], orientation: [1,1] }
-
-$orientations = [-1, 1].flat_map { |e1| [-1, 1].map { |e2| [e1, e2] } }
-
-def subsets(a, size, exclude=Set.new)
-  return Set.new([Set.new]) if size == 0
-
-  subsets = Set.new
-
-  a.each_with_index do |e, i|
-    unless exclude.include?(i)
-      remainders = subsets(a, size-1, exclude + [i])
-      remainders.each do |remainder|
-        subsets.add(remainder + [e])
-      end
-    end
+filename = "test.txt"
+dirs = [1, -1]
+num_beacons = 12
+$orientations = dirs.flat_map do |e1|
+  dirs.flat_map do |e2|
+    dirs.map { |e3| [e1, e2, e3] }
   end
-
-  subsets
 end
 
 def try_overlap(scanners, scanner_data, a_idx, b_idx, size)
   scanner_a = scanners[a_idx]
   scanner_b = scanners[b_idx]
 
-  subsets(scanner_a, 3).each do |as|
-    subsets(scanner_b, 3).each do |bs|
-      # relative to a
-      scanner_b_pos, orientation = try_match(as, bs)
-      if !scanner_b_pos.nil?
-        scanner_data[b_idx] << {
-          from: a_idx,
-          pos: scanner_b_pos,
-          orientation: orientation
-        }
-        scanner_data[a_idx] << {
-          from: b_idx,
-          pos: scanner_b_pos.map { |e| -e },
-          orientation: orientation
-        }
-        return
-      end
-    end
+  scanner_b_pos, orientation = try_match(scanner_a, scanner_b, size)
+  if !scanner_b_pos.nil?
+    scanner_data[b_idx] << {
+      from: a_idx,
+      pos: scanner_b_pos,
+      orientation: orientation
+    }
+    scanner_data[a_idx] << {
+      from: b_idx,
+      pos: scanner_b_pos.map { |e| -e },
+      orientation: orientation
+    }
   end
 end
 
-def try_match(as, bs)
+def try_match(as, bs, size)
   $orientations.each do |orientation|
-    bs.each do |first_b|
-      first_a = as.first
-      opposite = orientation.map { |e| -e }
-      b_pos = move(first_a, first_b, opposite)
-      b_abs = bs.map { |b| move(b_pos, b, orientation) }
+    as.each do |first_a|
+      bs.each do |first_b|
+        opposite = orientation.map { |e| -e }
+        b_pos = move(first_a, first_b, opposite)
+        b_abs = bs.map { |b| move(b_pos, b, orientation) }
 
-      if as.sort == b_abs.sort
-        return [b_pos, orientation]
+        inter = Set.new(b_abs).intersection(Set.new(as))
+        puts "inter: #{inter.count}" if inter.count > 1
+        if inter.count == size
+          puts inter.to_a.map { |a| a.join(",") }.join("\n")
+          return [b_pos, orientation]
+        end
       end
     end
   end
@@ -88,10 +68,23 @@ def overlap_all(scanners, scanner_data, size)
   end
 
   index_pairs.each do |i1, i2|
-    try_overlap(scanners, scanner_data, i1, i2, 3)
+    puts "try: #{i1}, #{i2}"
+    try_overlap(scanners, scanner_data, i1, i2, size)
   end
 end
 
-overlap_all(scanners, scanner_data, 3)
-#try_overlap(scanners, scanner_data, 0, 1, 3)
+def parse_input(filename)
+  parts = File.read(filename).split("\n\n")
+  parts.map do |part|
+    part.split("\n")[1..].map do |line|
+      line.split(",").map(&:to_i)
+    end
+  end
+end
+
+scanners = parse_input("test.txt")
+scanner_data = Array.new(scanners.length) { [] }
+scanner_data[0] << { from: 0, pos: [0,0,0], orientation: [1,1,1] }
+
+overlap_all(scanners, scanner_data, num_beacons)
 puts scanner_data.inspect
